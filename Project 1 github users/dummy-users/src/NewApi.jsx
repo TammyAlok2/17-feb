f7c6ed8b58b0c2834480b6a55975a74c89ad4866
TammyAlok2 <aloktamrakar2@gmail.com>
2023-04-29 14:41:33 +0530

    starting a new appi project

--- Project 1 github users/dummy-users/src/NewApi.jsx
+++ Project 1 github users/dummy-users/src/NewApi.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect,useState} from 'react'
 
 const NewApi = () => {
+    const [users,getUsers] =useState([]);
+const setUsers= async()=>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/photos/");
+    getUsers(await response.json());
+}
+
+    useEffect(()=>{
+        setUsers();
+    })
   return (
     <>
-      This is for the new api project
+   <div className='container'>
+{
+    users.map((curElem)=>{
+        return(
+            <>
+                <div className='title'>
+                    {
+                        curElem.title
+                    }
+                </div>
+                <div className='data'>{curElem.name}</div>
+            </>
+        )
+    })
+}
+   </div>
     </>
   )
 }
