85724868b8d8c1ff4b384d421bdf8ae75b259086
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-05-18 22:20:00 +0530

    some changes done in the insertion

---
+++ c++/Queue/Insertion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+    int data;
+    int front;
+    int rear;
+    int *arr;
+};
+
+void Traverse (Queue * )
+
+int main(){
+//making program of inserttion of queue
+
+    return 0;
+}
